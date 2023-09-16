9aec6e3d604dbeba3631d9138e05a095bf038ddf
osparking <jbpark03@gmail.com>
2023-09-16 16:12:47 +0900

    manipulate parameters and return value

--- src/main/java/aspects/LoggingAspect.java
+++ src/main/java/aspects/LoggingAspect.java
@@ -1,11 +1,13 @@
 package aspects;
 
+import java.util.Arrays;
 import java.util.logging.Logger;
 
 import org.aspectj.lang.ProceedingJoinPoint;
 import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 
+import model.Comment;
 import space.jbpark.utility.MyUtil;
 
 @Aspect
@@ -14,9 +16,25 @@ public class LoggingAspect {
 	
 	@Around("execution(* services.*.*(..))")
 	public Object log(ProceedingJoinPoint joinPoint) throws Throwable {
-		log.info("차단된 메소드 호출 전");
-		Object result = joinPoint.proceed();
-		log.info("차단된 메소드 호출 후 반환값 : " + result);
-		return result;
+		String calledMethod = joinPoint.getSignature().getName();
+		Object[] arguments = joinPoint.getArgs();
+		
+		var sb = new StringBuffer("차단된 메소드명: ");
+		sb.append(calledMethod);
+		sb.append(", 원래 인자목록: ");
+		sb.append(System.lineSeparator());
+		sb.append("\t");
+		sb.append(Arrays.asList(arguments));
+		sb.append(", 호출 전");
+		log.info(sb.toString());
+		
+		var comment = new Comment();
+		comment.setAuthor("보리스");
+		comment.setText("지바고 이야기");
+		Object[] myArgs = {comment};
+		
+		Object result = joinPoint.proceed(myArgs);
+		log.info("원래 메소드 실행 결과 반환값 : " + result);
+		return "실패";
 	}
 }
